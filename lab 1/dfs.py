6f8b97b4baa405583008dfb525124540f96ca2df
Varun <varun14g@gmail.com>
2025-07-29 15:32:42 +0530

    commit

--- lab 1/dfs.py
+++ lab 1/dfs.py
@@ -1,33 +1,29 @@
-def dfs(node, graph, visited, component):
-    component.append(node)
-    visited[node] = True
-    for child in graph[node]:
-        if not visited[child]:
-            dfs(child, graph, visited, component)
+def dfs(graph, start):
+    visited = set()
+    stack = [start]
 
-if __name__ == "__main__":
+    while stack:
+        node = stack.pop()
+        if node not in visited:
+            print(node)
+            visited.add(node)
 
-    n = int(input("Enter number of nodes: "))
-    graph = {}
+            # Add neighbors to the stack
+            # Reversed to maintain similar order as recursive DFS
+            for neighbor in reversed(graph[node]):
+                if neighbor not in visited:
+                    stack.append(neighbor)
 
-    for i in range(n):
-        edges = input(f"Enter adjacent nodes for node {i} (space-separated): ")
-        graph[i] = list(map(int, edges.strip().split()))
+    return visited
 
-    visited = {node: False for node in graph}
-    components = []
+graph = {
+    'A': ['B', 'C'],
+    'B': ['D', 'E'],
+    'C': ['F'],
+    'D': [],
+    'E': ['F'],
+    'F': []
+}
 
-    for node in graph:
-        if not visited[node]:
-            component = []
-            dfs(node, graph, visited, component)
-            components.append(component)
+dfs(graph, 'A')
 
-    for i, comp in enumerate(components):
-        print(f"Component {i+1} : {comp}")
-
-# 4
-# 0
-# 1 3
-# 1 2
-# 3
